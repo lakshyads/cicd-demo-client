e29fe3b28eb2288d8fdb212f696d53ebda95aaee
Lakshya Dev <lakshyadev@live.com>
2021-01-05 19:28:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [data, setData] = useState('backend is not connected');
+
+  useEffect(() => {
+    const url = 'https://cicd-backend.herokuapp.com/';
+    Axios.get('http://localhost:3002')
+      .then((response) => {
+        // console.log(response);
+        setData(response.data);
+      })
+      .catch((err) => {
+        setData('error from backend: ' + err.message);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          cicd-demo-frontend
         </a>
+        <p>Message from backend: {data}</p>
       </header>
     </div>
   );
